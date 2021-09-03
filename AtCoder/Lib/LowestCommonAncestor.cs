2f99588d3f3b0a316cf649107728d483a52a9eb9
Kentaro SHIRAICHI <shiraishi-k@pise.co.jp>
2021-09-03 09:38:54 +0900

    LCA実装

---
+++ AtCoder/Lib/LowestCommonAncestor.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Lib
+{
+	class LowestCommonAncestorTree
+	{
+		private readonly int n_;
+		private readonly List<int>[] parents_;
+		private readonly List<int>[] edges_;
+		private readonly int[] depthes_;
+		private readonly int top_;
+
+		public LowestCommonAncestorTree(List<int>[] edges, int top = 0)
+		{
+			edges_ = edges;
+			n_ = edges.Length;
+			parents_ = new List<int>[n_];
+
+			for (int i = 0; i < n_; i++)
+			{
+				parents_[i] = new List<int>();
+			}
+
+			depthes_ = new int[n_];
+			top_ = top;
+
+			DFS(top_, new List<int>(), new HashSet<int>());
+		}
+
+		private void DFS(int node, List<int> route, HashSet<int> yet)
+		{
+			if (yet.Contains(node))
+			{
+				return;
+			}
+
+			var depth = route.Count;
+			depthes_[node] = depth;
+
+			var k = 0;
+
+			while ((1 << k) <= depth)
+			{
+				var target = 1 << k;
+				var t = route[^target];
+				parents_[node].Add(t);
+				k++;
+			}
+
+			route.Add(node);
+			yet.Add(node);
+
+			foreach (var next in edges_[node])
+			{
+				DFS(next, route, yet);
+			}
+
+			route.RemoveAt(route.Count - 1);
+		}
+
+		public int Query(int node, int n)
+		{
+			if (n == 0)
+			{
+				return node;
+			}
+
+			var k = 1;
+			var count = 0;
+
+			while ((k << 1) <= n)
+			{
+				k <<= 1;
+				count++;
+			}
+
+			return Query(parents_[node][count], n - k);
+		}
+
+		public int GetRoot(int node1, int node2)
+		{
+			var (a, b) = depthes_[node1] > depthes_[node2]
+				? (node2, node1)
+				: (node1, node2);
+
+			b = Query(b, depthes_[b] - depthes_[a]);
+
+			if (a == b)
+			{
+				return a;
+			}
+
+			var ng = -1;
+			var ok = depthes_[a] + 1;
+
+			while (Math.Abs(ng - ok) > 1)
+			{
+				var c = (ng + ok) / 2;
+
+				if (Query(a, c + 1) == Query(b, c + 1))
+				{
+					ok = c;
+				}
+				else
+				{
+					ng = c;
+				}
+			}
+
+			return Query(a, ok + 1);
+		}
+
+		public int GetDepth(int node)
+		{
+			return depthes_[node];
+		}
+	}
+}
