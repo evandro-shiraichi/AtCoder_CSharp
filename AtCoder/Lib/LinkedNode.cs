30c551399e47826456f1852d73ddb5882044c8a1
Evandro Sylos <c0115178d1@edu.teu.ac.jp>
2021-10-31 12:59:21 +0900

    Degreeと区間スケジュール、LinkedNode追加 Dequeue修正

---
+++ AtCoder/Lib/LinkedNode.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder.Lib {
+
+	class LinkedNode<T> : IEnumerable<T> {
+		public T Value { get; set; }
+
+		public LinkedNode<T> Next { get; private set; }
+
+		public LinkedNode<T> Previous { get; private set; }
+
+		public LinkedNode(T v) {
+			Value = v;
+		}
+
+		public void AddAfter(LinkedNode<T> next) {
+			if (next.Previous is null == false || Next is null == false) {
+				throw new Exception();
+			}
+
+			next.Previous = this;
+			Next = next;
+		}
+
+		public void AddBefore(LinkedNode<T> previous) {
+			if (previous.Next is null == false || Previous is null == false) {
+				throw new Exception();
+			}
+
+			previous.Next = this;
+			Previous = previous;
+		}
+
+		public void DislinkAfter() {
+			if (Next is null == false)
+				Next.Previous = null;
+
+			Next = null;
+		}
+
+		public void DislinkBefore() {
+			if (Previous is null == false)
+				Previous.Next = null;
+
+			Previous = null;
+		}
+
+		public void Dislink() {
+			DislinkAfter();
+			DislinkBefore();
+		}
+
+		private IEnumerable<T> Enumerate() {
+			var statck = new Stack<T>();
+
+			var previous = Previous;
+
+			while (previous is null == false) {
+				statck.Push(previous.Value);
+				previous = previous.Previous;
+			}
+
+			foreach (var temp in statck) {
+				yield return temp;
+			}
+
+			yield return Value;
+
+			var next = Next;
+
+			while (next is null == false) {
+				yield return next.Value;
+				next = next.Next;
+			}
+		}
+
+		public IEnumerator<T> GetEnumerator() {
+			return Enumerate().GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return Enumerate().GetEnumerator();
+		}
+	}
+}
---
+++ AtCoder/Lib/Scheduling.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtCoder.Lib {
+	class RangeScheduling<T> where T : IComparable<T> {
+		private List<(T start, T end)> list_ = new List<(T start, T end)>();
+
+		public RangeScheduling() : this(new List<(T, T)>()) { }
+
+		public RangeScheduling(IList<(T start, T end)> list) {
+			list_ = list.ToList();
+		}
+
+		public void Add(T start, T end) {
+			list_.Add((start, end));
+		}
+
+		public void Add((T start, T end) tuple) {
+			list_.Add(tuple);
+		}
+
+		public IEnumerable<(T start, T end)> Calculate(bool includingBorder = true) {
+			var border = includingBorder ? 0 : 1;
+			list_.Sort((x, y) => x.end.CompareTo(y.end));
+
+			var (last, _) = list_.FirstOrDefault();
+
+			foreach (var (start, end) in list_) {
+				if (start.CompareTo(last) >= border) {
+					yield return (start, end);
+					last = end;
+				}
+			}
+		}
+	}
+
+}
